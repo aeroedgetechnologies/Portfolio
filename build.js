13407761ca7de94fe42ab0ffd314c07544e3b1f4
aeroedgetechnologies <aeroedgetechnologies@gmail.com>
2025-06-28 07:30:57 -0700

    hi

--- build.js
+++ build.js
@@ -1,6 +1,10 @@
-const { build } = require('vite');
-const path = require('path');
-const fs = require('fs');
+import { build } from 'vite';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function buildApp() {
   try {
