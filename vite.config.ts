902774fe9ba12c474814684172c9b953e52a8bc8
aeroedgetechnologies <aeroedgetechnologies@gmail.com>
2025-06-28 06:57:20 -0700

    hi

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,11 @@ export default defineConfig({
     port: 3006,
     proxy: {
       '/api': {
-        target: 'http://localhost:5003',
+        target: 'https://forgetworries.onrender.com',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:5003',
+        target: 'https://forgetworries.onrender.com',
         ws: true,
       },
     },
